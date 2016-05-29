538b02326d0064dae6c2816c812c7de7a77980f3
Rick Chen <rick@rickfrom1987.com>
2016-05-28 18:26:02 -0700

    update

--- tools/deploy.s3.js
+++ tools/deploy.s3.js
@@ -12,14 +12,15 @@ module.exports = task('deploy', () => Promise.resolve()
   .then(() => new Promise((resolve, reject) => {
     const client = s3.createClient({
       s3Options: {
-        region: 'us-east-1',
+        accessKeyId:'AKIAIFCC7DJF424AHI5A',
+        secretAccessKey: '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',
         sslEnabled: true,
       },
     });
     const uploader = client.uploadDir({
       localDir: 'build',
       deleteRemoved: true,
-      s3Params: { Bucket: 'www.example.com' },
+      s3Params: { Bucket: 'rickfrom1987.com' },
     });
     uploader.on('error', reject);
     uploader.on('end', resolve);
