f9999b62e24342b1f7bd025714f1ae3351778834
Rick Chen <rick@rickfrom1987.com>
2016-05-29 18:00:21 -0700

    remove redirects

--- routes/index.js
+++ routes/index.js
@@ -7,21 +7,6 @@ import error from './error';
 const routes = [
   home,
   projects,
-  { 
-    path: '*', 
-    action({ history }) {
-      return new Promise((resolve, reject) => {
-        const pathname = window.location.pathname;
-        const isRedirect = (pathname.indexOf('/0/') === 0);
-        if (!isRedirect) {
-          return history.push('/');
-        } else {
-          const redirectPath = pathname.replace('0/', '');
-          return history.push(redirectPath);
-        }
-      });
-    } 
-  }
 ];
 
 export default routes;
